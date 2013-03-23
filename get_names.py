8e21a71ab89d5de1a0508bf9fc247797d503c445
process <justin.chines@gmail.com>
2013-03-23 03:47:03 -0400

    Added output file support. Improved error handling.

--- get_names.py
+++ get_names.py
@@ -3,36 +3,54 @@ import itertools
 import eventlet
 from eventlet.green import httplib
 
+# Globals
+
 CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789_'
-USAGE = """Usage: %s [--brute N] [--dict FILE]
+USAGE = """Usage: %s [--brute N] [--dict FILE] [--out FILE]
   --brute N                Tries all possible names of length N
-  --dict FILE              Tries all possible names listed in the given file
+  --dict FILE              Tries names listed in the given file
+  --out FILE               Stores all available names in the given file
 
 Either --brute or --dict MUST be specified."""
-POOL_SIZE = 10
+POOL_SIZE = 5
+
+# Helper function definitions
 
 def brute_force_names(length):
   return itertools.product(CHARS, repeat=length)
 
 def get_http_status(name):
+  try:
     c = httplib.HTTPSConnection("twitter.com")
     c.request("HEAD", "/%s" % name)
     return c.getresponse().status, name
+  except httplib.HTTPException as e:
+    return -1, name
 
 def tuple_to_string(tup):
   return ''.join(tup)
 
+# Parse command line arguments
+if '--out' in sys.argv:
+  i = sys.argv.index('--out')
+  outfile = open(sys.argv[i+1], 'w')
+else:
+  outfile = None
+
 if '--brute' in sys.argv:
-  name_length = int(sys.argv[2])
+  i = sys.argv.index('--brute')
+  name_length = int(sys.argv[i+1])
   names = brute_force_names(name_length)
   using_dict = False
 elif '--dict' in sys.argv:
-  f = open(sys.argv[2])
+  i = sys.argv.index('--dict')
+  f = open(sys.argv[i+1])
   names = f.read().split()
   using_dict = True
 else:
   print USAGE % sys.argv[0]
   sys.exit(-1)
+
 
 # Just some stats variables
 names_taken = 0
@@ -46,6 +64,7 @@ else:
   total_names = len(CHARS) ** name_length
   names = [tuple_to_string(name) for name in names]
 
+# Let's do it!
 print "Trying %d names..." % total_names
 
 thread_pool = eventlet.GreenPool(POOL_SIZE)
@@ -56,6 +75,8 @@ for status, name in thread_pool.imap(get_http_status, names):
     print "Name available: " + name
     names_available += 1
     available_names.append(name)
+    if outfile:
+      outfile.write(name+'\n')
 
   # 200 = OK. It's taken.
   elif status == 200:
@@ -65,8 +86,12 @@ for status, name in thread_pool.imap(get_http_status, names):
   elif status == 302:
     names_suspended += 1
 
+  # -1 = httplib error.
+  elif status == -1:
+    print "Error occurred while checking " + name
+
   else:
-    print "Bad code. Name: %s; Code: %d" % (name, status)
+    print "Unexpected code. Name: %s; Code: %d" % (name, status)
 
 # Print results
 print str(total_names) + " total names checked"
